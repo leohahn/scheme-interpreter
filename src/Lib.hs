64dde8dd82f8d96ae9a68679a72bc45f61fb0398
Leonardo Hahn <leo_lh324@hotmail.com>
2016-02-06 00:58:41 +0100

    basic type evaluation and binary operators

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,25 +20,43 @@ data LispVal
     | String String
     | Char Char
     | Bool Bool
-    deriving (Show)
+
+instance Show LispVal where
+    show = showVal
+
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Float contents) = show contents
+showVal (Char contents) = "#\\" ++ [contents]
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList first second) =
+    "(" ++ unwordsList first ++ " . " ++ showVal second ++ ")"
+
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 
 -- | Entry point of the application, gets arguments and evaluates it with
 -- the readExpr function
 entry :: IO ()
 entry =
-    do args <- getArgs
-       putStrLn $ readExpr (head args)
+    getArgs >>= putStrLn . show . eval . readExpr . head
 
 
 -- | Given a string, reads the expression and evaluates it
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-                   Left err -> "No match: " ++ show err
-                   Right val -> "Found value: " ++ show val
+                   Left err -> String $ "No match: " ++ show err
+                   Right val -> val
 
 
--- | Parses the whole expression, returning the evaluated value
+-- | Parses the whole expression, returning the value
 parseExpr :: Parser LispVal
 parseExpr = parseHash
         <|> parseNumberBase 'd'
@@ -48,15 +66,16 @@ parseExpr = parseHash
         <|> parseAnyList
 
 
+-- | Parses both normal lists or dotted lists
 parseAnyList :: Parser LispVal
 parseAnyList =
     do char '('
-       firstPart <- sepEndBy parseExpr spaces
-       spacesOpt
+       firstPart <- sepEndBy parseExpr spaces1
+       spaces
        sep <- choice [char '.',char ')']
        case sep of
          '.' ->
-             do spaces
+             do spaces1
                 secondPart <- parseExpr
                 char ')'
                 return (DottedList firstPart secondPart)
@@ -64,21 +83,6 @@ parseAnyList =
              return (List firstPart)
          _   ->
              undefined
-
-spacesOpt :: Parser ()
-spacesOpt = skipMany space
-
--- | Parses a list with expressions separated by spaces
-parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
-
-
--- | Parses a list in its dotted form
-parseDottedList :: Parser LispVal
-parseDottedList =
-    do x <- endBy parseExpr spaces
-       xs <- char '.' >> spaces >> parseExpr
-       return $ DottedList x xs
 
 
 -- | Parses a LispVal that is quoted
@@ -149,7 +153,7 @@ parseChar =
        return $ Char c
 
 
--- | Parses a number at a specific base
+-- | Parses a number at a specific base (Need refactoring, it is ugly)
 parseNumberBase :: Char -> Parser LispVal
 parseNumberBase 'b' =
     do digits <- many1 (oneOf "01")
@@ -186,6 +190,7 @@ symbol :: Parser Char
 symbol = oneOf "!$%&*+-./:<=?>@^_~"
 
 
+-- | Defines how a symbol should begin
 initial :: Parser Char
 initial = letter <|> specialInitial
 
@@ -195,6 +200,7 @@ specialInitial =
     oneOf ['!','$','%','&','*','/',':','<','=','>','?','^','_','~']
 
 
+-- | Defines which characters are allowed in the middle of a identifier
 subsequent :: Parser Char
 subsequent = initial <|> digit <|> specialSubsequent
 
@@ -208,5 +214,64 @@ peculiarIdentifier = oneOf ['+','-']
 
 
 -- | Removes all spaces until other character is found
+spaces1 :: Parser ()
+spaces1 = skipMany1 space
+
+-- | Removes all spaces if any exists, otherwise do nothing
 spaces :: Parser ()
-spaces = skipMany1 space
+spaces = skipMany space
+
+----------------------------------------------------------
+-- Evaluation
+----------------------------------------------------------
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Float _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+
+primitives :: [(String,[LispVal] -> LispVal)]
+primitives = [ ("+", numericBinop (+))
+             , ("-", numericBinop (-))
+             , ("*", numericBinop (*))
+             , ("/", numericBinop (*))
+             , ("mod", numericBinop mod)
+             , ("quotient", numericBinop quot)
+             , ("remainder", numericBinop rem)
+             , ("string?", isString)
+             , ("float?", isFloat)
+             ]
+
+
+isString :: [LispVal] -> LispVal
+isString [String _] = Bool True
+isString _ = Bool False
+
+
+isFloat :: [LispVal] -> LispVal
+isFloat [Float _] = Bool True
+isFloat _ = Bool False
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op args = Number $ foldl1 op (map unpackNum args)
+
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String s) =
+    if null parsed
+        then 0
+        else fst $ (head parsed)
+    where
+      parsed = reads s
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
